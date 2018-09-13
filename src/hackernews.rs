83fb5572ae3115c9fa1d2d87c6b7577653229930
kingdido999 <info@desmonding.me>
2018-09-13 21:29:35 +0800

    Map top story ids to stories

--- src/hackernews.rs
+++ src/hackernews.rs
@@ -2,6 +2,8 @@ extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 
+use std::error::Error;
+
 pub struct HackerNews {
   client: reqwest::Client,
 }
@@ -13,7 +15,7 @@ impl HackerNews {
     }
   }
 
-  pub fn fetch_top_story_ids(&self) -> Result<Vec<u64>, Box<std::error::Error>> {
+  pub fn fetch_top_story_ids(&self) -> Result<Vec<u64>, Box<Error>> {
     let res = self
       .client
       .get("https://hacker-news.firebaseio.com/v0/topstories.json")
@@ -23,11 +25,22 @@ impl HackerNews {
     Ok(top_stories)
   }
 
-  pub fn fetch_item_by_id(&self, id: u64) -> Result<Item, Box<std::error::Error>> {
+  pub fn fetch_item_by_id(&self, id: u64) -> Result<Item, Box<Error>> {
     let url = format!("https://hacker-news.firebaseio.com/v0/item/{}.json", id);
     let res = self.client.get(&*url).send()?.text()?;
     let item: Item = serde_json::from_str(&res)?;
     Ok(item)
+  }
+
+  pub fn fetch_top_stories(&self) -> Result<Vec<Item>, Box<Error>> {
+    // TODO: limit the number of stories to fetch
+    let top_story_ids = self.fetch_top_story_ids().unwrap();
+    let top_stories: Vec<Item> = top_story_ids
+      .into_iter()
+      .map(|id| self.fetch_item_by_id(id).unwrap())
+      .collect();
+
+    Ok(top_stories)
   }
 }
 
@@ -74,5 +87,12 @@ mod tests {
       item.url.unwrap(),
       String::from("http://www.getdropbox.com/u/2/screencast.html")
     );
+  }
+
+  #[test]
+  fn it_fetches_top_stories() {
+    let hn = HackerNews::new();
+    let top_stories = hn.fetch_top_stories().unwrap();
+    assert!(top_stories.len() > 0);
   }
 }
